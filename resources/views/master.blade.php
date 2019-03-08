39cd385fb0e7970256f18dd2bf6e2be8779cdfb3
andyharyanto <andy.haryanto@ralali.com>
2019-03-08 17:02:40 +0700

    adding styling

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,15 +14,52 @@
   <!-- Styles -->
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-
+<style>
+  *{
+      margin: 0;
+      padding: 0%;
+      font-family: Arial, Helvetica;
+  }
+  nav{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+      background-color: orange;
+      color: white;
+      padding: 10px 0;
+  }
+  .login-container{
+      display: flex;
+      align-items: center;
+  }
+  .login-container > button{
+      background-color:darkcyan;
+      border: none;
+      padding: 5px 10px;
+      color: white;
+      margin: 0 5px;
+      border-radius: 2px;
+      transition: all 0.3s ease;
+  }
+  .login-container > button:hover{
+      background-color: antiquewhite;
+      color: black;
+      cursor: pointer;
+  }
+  
+</style>
 <body>
-  <header class="site-header">
-    <h1>Ralali Bootcamp</h1>
-    <form class="search-bar">
-      <input type="text" name="keyword">
-      <button type="submit">Cari</button>
-    </form>
-  </header>
+  <header>
+    <nav>
+            <h1>Ralali Bootcamp</h1>
+            <br>
+            <input type="text" placeholder="Search">
+            <div class="login-container">
+                <button id="signIn">Sign In</button>
+                <button id="signOut">Sign Out</button>
+            </div>
+    </nav>
+</header>
 
   @yield('content')
 </body>
