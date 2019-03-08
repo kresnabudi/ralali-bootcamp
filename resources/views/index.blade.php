d25af0d2376de57ac761126307d87009ad38f77f
indratamagiri <madegiri1212@gmail.com>
2019-03-08 16:28:16 +0700

    GG WP

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
 @extends('master')
 @section('content')
-  Lorem ipsum dolor sit amet
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, corrupti. Suscipit porro exercitationem odit sequi possimus, nam cumque, minima officiis accusamus quibusdam corporis? Laboriosam, nostrum blanditiis magnam reiciendis libero recusandae.
 @endsection
